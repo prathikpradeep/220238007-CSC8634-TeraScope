32da973ac498087de4c7cb6b688e0c34d68424ca
Prathik Pradeep <prathik.98@gmail.com>
2023-01-27 01:32:55 +0000

    Second question done

--- Cloud_Computing_TeraScope_Assignment/munge/01-A.R
+++ Cloud_Computing_TeraScope_Assignment/munge/01-A.R
@@ -1,53 +1,47 @@
-# library(ggplot2)
-# library(dplyr)
-# library(tidyverse)
-# library(lubridate)
-# library(purrr)
-# library(data.table)
-# library(knitr)
-# library(kableExtra)
-#library(ggpubr)
-#
-# 
-# application_checkpoints = read.csv("application_checkpoints.csv")
-# gpu = read.csv("gpu.csv")
-# task_x_y = read.csv("task_x_y.csv")
+# setwd("/Volumes/Prathik/MSc Data Science/Semester 1/Block 3/Cloud Computing - CSC8634/Assignment/Assignment/220238007-CSC8634-TeraScope/Cloud_Computing_TeraScope_Assignment/data")
+# application_checkpoints <- read.csv("application_checkpoints.csv")
 
-
-
-# Taking a sample of code
-# application_checkpoints_filtered <- data.frame()
-# 
-# for (j in 1:5) {
-#   
-#   taskid <- unique(application_checkpoints$taskId)[j]
-#   
-#   temp <- application_checkpoints %>%
-#     filter(taskId == taskid)
-#   
-#   application_checkpoints_filtered <- rbind(application_checkpoints_filtered,temp)
-# }
-
-
+#Setting seed so results are reproducible
+set.seed(220238007)
 
 ####### Question 1
 ## Pre-processing for application_checkpoints data
 #Converting the timestamp in application_checkpoints to the correct format and datatype
 options(digits.secs=3)
 application_checkpoints$timestamp <- ymd_hms(application_checkpoints %>%
-                                               mutate(time_clean = lubridate::ymd_hms(timestamp)) %>% 
+                                               mutate(time_clean = lubridate::ymd_hms(timestamp)) %>%
                                                mutate(date = format(time_clean,format =  '%F %H:%M:%OS')) %>%
                                                dplyr::pull(date))
 
 ## Pre-processing for gpu data
 #Converting the timestamp in gpu to the correct format and datatype
 gpu$timestamp <- ymd_hms(gpu %>%
-                           mutate(time_clean = lubridate::ymd_hms(timestamp)) %>% 
+                           mutate(time_clean = lubridate::ymd_hms(timestamp)) %>%
                            mutate(date = format(time_clean,format =  '%F %H:%M:%OS')) %>%
                            dplyr::pull(date))
 
 #Converting gpuSerial from factor to character
 gpu$gpuSerial = as.character(gpu$gpuSerial)
+
+
+# #Taking a sample of code
+# application_checkpoints <- application_checkpoints %>%
+#   setorder(-jobId)
+# 
+# application_checkpoints_filtered <- data.frame()
+# 
+# for (j in 1:300)
+#   {
+#   taskid <- unique(application_checkpoints$taskId)[j]
+#   temp <- application_checkpoints %>%
+#     filter(taskId == taskid)
+#   application_checkpoints_filtered <- rbind(application_checkpoints_filtered,temp)
+# }
+# application_checkpoints <- application_checkpoints_filtered
+
+
+
+
 
 ## Merging Application Checkpoints and GPU on hostname and similar timestamps
 # Sample data
@@ -62,6 +56,7 @@ setkey(gpu, time)
 
 # Rolling join by nearest time
 gpu_checkpoints <- gpu[application_checkpoints, on = .(hostname, time), roll = "nearest"]
+
 
 ##Converting gpu_checkpoints to a dataframe
 gpu_checkpoints <- data.frame(gpu_checkpoints)
@@ -90,7 +85,7 @@ gpu_checkpoints <- gpu_checkpoints %>%
 gpu_checkpoints <- gpu_checkpoints %>%
  spread(eventType,timestamp)
 
-#Computing Event Time in milliseconds 
+#Computing Event Time in milliseconds
 gpu_checkpoints$eventTime_in_ms <- round(as.numeric(difftime(gpu_checkpoints$STOP, gpu_checkpoints$START,units ="secs"))*1000, 3)
 
 #Removing the columns grouped_id, Start and Stop
@@ -99,4 +94,10 @@ gpu_checkpoints <- gpu_checkpoints[,!names(gpu_checkpoints) %in% c("grouped_id",
 
 
 ####### Question 2
+#Joining task_x_y to gpu_checkpoints table
+gpu_checkpoints_task <- gpu_checkpoints %>%
+                          left_join(task_x_y,by=c("taskId","jobId"))
 
+#Filtering out only TotalRender
+gpu_checkpoints_task <- gpu_checkpoints_task %>%
+  filter(eventName == "TotalRender")
--- Cloud_Computing_TeraScope_Assignment/src/eda.R
+++ Cloud_Computing_TeraScope_Assignment/src/eda.R
@@ -1,3 +1,4 @@
+##### Question 1
 gpu_checkpoints %>%
   group_by(eventName)%>%
   summarise(med = mean(powerDrawWatt)) %>%
@@ -31,6 +32,51 @@ gpu_checkpoints %>%
   geom_bar(stat = "identity") +
   geom_text(aes(label=round(med, digits = 0)), vjust=1.2, color="red", size=3.5)+
   scale_fill_gradient(low = "lightblue", high = "darkblue") +
+  guides(fill=guide_legend(title="Event Time")) +
   ggtitle("Event Name vs Event Time in ms") +
   labs(x = "Event Name", y = "Event Time in ms") + 
+  theme_minimal()
+
+
+
+##### Question 2
+#Graphical Summary of Zoom Level vs Power Draw in Watts
+gpu_checkpoints_task %>%
+  group_by(level)%>%
+  summarise(med = mean(powerDrawWatt)) %>%
+  ggplot(aes(x = reorder(level, +med), y = med, fill = med)) +
+  geom_bar(stat = "identity") +
+  geom_text(aes(label=round(med, digits = 0)), vjust=1.2, color="red", size=3.5)+
+  scale_fill_gradient(low = "lightblue", high = "darkblue") +
+  guides(fill=guide_legend(title="Power Draw")) +
+  ggtitle("Zoom Level vs Power Draw in Watts") +
+  labs(x = "Zoom Level", y = "Power Draw in Watts") + 
+  theme_minimal()
+
+
+#Graphical Summary of Zoom Level vs GPU Temperature in C
+gpu_checkpoints_task %>%
+  group_by(level)%>%
+  summarise(med = mean(gpuTempC)) %>%
+  ggplot(aes(x = reorder(level, +med), y = med, fill = med)) +
+  geom_bar(stat = "identity") +
+  geom_text(aes(label=round(med, digits = 0)), vjust=1.2, color="red", size=3.5)+
+  scale_fill_gradient(low = "lightblue", high = "darkblue") +
+  guides(fill=guide_legend(title="GPU Temp")) +
+  ggtitle("Zoom Level vs GPU Temperature in C") +
+  labs(x = "Zoom Level", y = "GPU Temperature in C") + 
+  theme_minimal()
+
+
+#Graphical Summary of Zoom Level vs Event Time in ms
+gpu_checkpoints_task %>%
+  group_by(level)%>%
+  summarise(med = mean(eventTime_in_ms)) %>%
+  ggplot(aes(x = reorder(level, +med), y = med, fill = med)) +
+  geom_bar(stat = "identity") +
+  geom_text(aes(label=round(med, digits = 0)), vjust=1.2, color="red", size=3.5)+
+  scale_fill_gradient(low = "lightblue", high = "darkblue") +
+  guides(fill=guide_legend(title="Event Time")) +
+  ggtitle("Zoom Level vs Event Time in ms") +
+  labs(x = "Zoom Level", y = "Event Time in ms") + 
   theme_minimal()
